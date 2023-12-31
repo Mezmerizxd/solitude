8bc85257e2d95c4790839da97da4802bb2670a43
mezmerizxd <rorypearson2003@hotmail.com>
2023-12-31 23:44:56 +0000

    Update tsconfig and package.json, add particle animation to landing page

--- src/client/src/routes/index.tsx
+++ src/client/src/routes/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { useRoutes } from 'react-router-dom';
-import StarfieldAnimation from 'react-starfield-animation';
+import { lazyImport } from '../libs/lazyImport';
+
+import Particles, { initParticlesEngine } from '@tsparticles/react';
+import { type Container, type ISourceOptions, MoveDirection, OutMode } from '@tsparticles/engine';
+import { loadSlim } from '@tsparticles/slim';
 
 const { Landing } = lazyImport(() => import('../features/home'), 'Landing');
 const { Components } = lazyImport(() => import('../features/home'), 'Components');
 
-import { lazyImport } from '../libs/lazyImport';
-
 export const AppRoutes = () => {
+  const [init, setInit] = React.useState(false);
+
   const commonRoutes = [
     { path: '/', element: <Landing /> },
     {
@@ -18,19 +22,90 @@ export const AppRoutes = () => {
 
   const element = useRoutes([...commonRoutes]);
 
+  React.useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      //await loadAll(engine);
+      //await loadFull(engine);
+      await loadSlim(engine);
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    // console.log(container);
+  };
+
+  const options: ISourceOptions = React.useMemo(
+    () => ({
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: 'slow',
+          },
+          onHover: {
+            enable: true,
+            mode: 'slow',
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: '#D9FF4B',
+        },
+        links: {
+          color: '#BDDE43',
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        move: {
+          direction: 'none',
+          enable: true,
+          outModes: {
+            default: 'out',
+          },
+          random: true,
+          speed: 2,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 80,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: 'circle',
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true,
+    }),
+    [],
+  );
+
   return (
     <div className="text-white-light bg-black">
-      <StarfieldAnimation
-        style={{
-          position: 'fixed',
-          width: '100%',
-          height: '100%',
-          pointerEvents: 'none',
-        }}
-        numParticles={400}
-        dx={0.05}
-        dy={0.05}
-      />
+      {init && <Particles id="tsparticles" particlesLoaded={particlesLoaded} options={options} />}
       {element}
     </div>
   );
